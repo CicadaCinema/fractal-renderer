717f9cdc8917e08784809c3de559abdad4407a16
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2024-08-08 18:38:16 +0100

    produce debbuging information while compiling

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm bin
-gcc -Wall -I/usr/include/ -c -o main.o main.cpp
-gcc -Wall -I/usr/include/ -o bin -L/usr/X11R6/lib main.o -lX11 -lXi -lXmu -lglut -lGL -lGLU -lm -lGLEW
+gcc -g -Wall -I/usr/include/ -c -o main.o main.cpp
+gcc -g -Wall -I/usr/include/ -o bin -L/usr/X11R6/lib main.o -lX11 -lXi -lXmu -lglut -lGL -lGLU -lm -lGLEW
 rm main.o
 ./bin
