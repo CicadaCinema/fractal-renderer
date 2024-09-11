173b4bf92b6b4fa49ac9ea4d9c7b0a92d6f85a60
Arseny <git@arseny.uk>
2024-09-11 21:57:52 +0100

    we actually do not need to call `newrender` every tick as it is called at the beginning via `topLevelInit`>`initiateIFS`>`newsetup`>`newrender`

--- main.cpp
+++ main.cpp
@@ -2512,7 +2512,6 @@ void topLevelDisplay() {
   // NOTE THAT WE ARE LOOPING OVER THIS FUNCTION MANY TIMES!!
   // FIXME: only initiate iteration parameters when we actually start a new
   // render!
-  newrender();
   for (int i___ = 0; i___ < 2000; i___++) {
     DoMyStuff();
   }
